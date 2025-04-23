0ccea112a196f25a7fbcf04a56ff5a465b0fd858
MauMau221 <mauriciobgsp@gmail.com>
2025-04-22 21:47:01 -0300

    Exercicio 1 feito

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        PessoaFisica pessoaFisica = new PessoaFisica();
+        pessoaFisica.setNome("Mauricio");
+        pessoaFisica.setEndereco("Rua tal");
+        pessoaFisica.setCpf("44444444");
+        pessoaFisica.juntar();
+        System.out.println(pessoaFisica.getCpf());
+        
+        PessoaJuridica pessoaJuridica = new PessoaJuridica();
+        pessoaJuridica.setNome("Empresa");
+        pessoaJuridica.setEndereco("Rua tal");
+        pessoaJuridica.setCnpj("0000001");
+        pessoaJuridica.juntar();
+        System.out.println(pessoaJuridica.getCnpj());
+       
+    }
+}
